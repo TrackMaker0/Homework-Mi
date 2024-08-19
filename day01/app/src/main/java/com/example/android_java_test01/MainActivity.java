285458d7379d3ade83a7dd8face063c26a0c1865
谢海龙安卓240108 <2315674401@xiaomi.com>
2024-08-19 22:47:49 +0800

    Day01

---
+++ day01/app/src/main/java/com/example/android_java_test01/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.android_java_test01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    public void onStateNotSaved() {
+        super.onStateNotSaved();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+}
+
+
